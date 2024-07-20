a41b46d4cd45bd1757bc65f64be8e9018f5e83b2
CtrTemp <1744909116@qq.com>
2024-07-20 13:03:26 +0800

    modify dev1_1

--- newFunc.py
+++ newFunc.py
@@ -3,10 +3,12 @@ import uuid
 
 def add_dev1(a, b):
     print("modify dev1")
+    print("modify dev1_1")
     return a+b
 
 
 
 def add_dev2(a, b):
     print("modify dev2")
+    print("modify dev1_1")
     return a+b
